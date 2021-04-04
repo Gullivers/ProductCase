b7e1ef10feafffcb285af2c599cc8ea05a3e5750
Dogan Akdag <dogan@gullivers.games>
2021-04-04 19:39:59 +0300

    Case Project Ready

---
+++ ProductCase/Assets/_Others/GG_CharMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GG_CharMove : MonoBehaviour
+{
+    public float m_Speed = 5f;
+    void Update()
+    {
+
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        transform.GetComponent<Rigidbody>().MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
+
+        // if (Input.GetKeyDown(KeyCode.UpArrow))
+        // {
+            
+        // }
+
+    }
+}
